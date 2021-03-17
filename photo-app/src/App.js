d06ded2f1482332be26aa72b038a639e29d7a6b2
Matt2844 <matt.hd.ford@gmail.com>
2021-03-17 19:36:47 -0400

    basic structure done ie file upload and BE

--- photo-app/src/App.js
+++ photo-app/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import ImageGrid from './components/ImageGrid';
 import Title from './components/Title'
 import UploadForm from './components/UploadForm';
+
 
 function App () {
   return (
     <div className="App">
       <Title />
       <UploadForm />
+      <ImageGrid />
     </div>
   );
 }
--- photo-app/src/components/UploadForm.js
+++ photo-app/src/components/UploadForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import ProgressBar from './ProgressBar';
 
 export default function UploadForm () {
   const [file, setFile] = useState(null);
@@ -21,7 +22,10 @@ export default function UploadForm () {
 
   return (
     <form>
-      <input type="file" onChange={changeHandler} />
+      <label>
+        <input type="file" onChange={changeHandler} />
+        <span>+</span>
+      </label>
       <div className="output">
         {error && (
           <div className="error">{error}</div>
@@ -29,7 +33,10 @@ export default function UploadForm () {
         {file && (
           <div>{file.name}</div>
         )}
+        {file && (
+          <ProgressBar file={file} setFile={setFile} />
+        )}
       </div>
-    </form>
+    </form >
   )
 }
