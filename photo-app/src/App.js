c3fa297057dbbf0686d20f384ebb8031b1ec0a54
Matt2844 <matt.hd.ford@gmail.com>
2021-03-17 17:05:12 -0400

    setup project and file upload

---
+++ photo-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Title from './components/Title'
+import UploadForm from './components/UploadForm';
+
+function App () {
+  return (
+    <div className="App">
+      <Title />
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ photo-app/src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Title () {
+  return (
+    <div className="title">
+      <h1>Firegram</h1>
+      <h2>Your Pictures</h2>
+      <p>Lorem Ipsum Something Someting</p>
+    </div>
+  )
+}
