c4bd985aadf9005051440722dff5dea84fd11819
Matt2844 <matt.hd.ford@gmail.com>
2021-03-19 14:51:57 -0400

    added navbar

--- photo-app/src/App.js
+++ photo-app/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import ImageGrid from './components/ImageGrid';
 import Title from './components/Title'
 import UploadForm from './components/UploadForm';
-
+import NavBar from './components/NavBar';
 
 function App () {
   return (
     <div className="App">
+      <NavBar />
       <Title />
       <UploadForm />
       <ImageGrid />
--- photo-app/src/components/Title.js
+++ photo-app/src/components/Title.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
+
 export default function Title () {
   return (
     <div className="title">
       <h1>Photo App</h1>
       <h2>Upload An Image</h2>
-      <p>Backend built with Firebase</p>
+      <p>This is a prototype.</p>
     </div>
   )
 }
